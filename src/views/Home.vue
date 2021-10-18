933a2e2c0ae5d471b73d724fbcc0168530661a4d
VitoriaIzaine <86832458+VitoriaIzaine@users.noreply.github.com>
2021-10-18 08:27:47 -0300

    Primeiro commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="home">
+      <div id="titulo">
+        <h1>
+          Bem vindo a Pesquisa <br />
+          de Satisfação
+        </h1>
+        <h4>Dê seu feedback sobre o colégio.</h4>
+        <button id="botao" type="button">Acessar Formulario</button>
+      </div>
+      <div id="barra"></div>
+      <div>
+        <main class="Formulario">
+          <section class="texto">
+            <div id="form">
+              <h1>Como é feito o formulário?</h1>
+              <h2>
+                Acessando o formulário, você irá se deparar com uma lista de
+                itens relacionados ao colégio, onde poderá opinar sua satisfação
+                sobre um determinado tópico e se ele é relevante para a
+                instituição.
+              </h2>
+            </div>
+          </section>
+        </main>
+
+        <main class="Formulario1">
+          <section class="texto">
+          <div id="form">
+            <h1>Para que ele serve?</h1>
+            <h2>
+              A Pesquisa de Satisfação é utilizada pela instituição para se
+              obter uma análise facilitada de como a qualidade de ensino e a
+              organização estrutural do colégio é vista pelos alunos.
+            </h2>
+          </div>
+          </section>
+        </main>
+
+        <main class="Formulario2">
+          <section class="texto">
+          <div id="form">
+            <h1>Para que ele serve?</h1>
+            <h2>
+              A Pesquisa de Satisfação é utilizada pela instituição para se
+              obter uma análise facilitada de como a qualidade de ensino e a
+              organização estrutural do colégio é vista pelos alunos.
+            </h2>
+          </div>
+          </section>
+        </main>
+
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
 </script>
+
+<style scoped src="@/views/scss/home.css">
+</style>
